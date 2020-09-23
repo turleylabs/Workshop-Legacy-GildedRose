10ad5f438a894d2c530413a15a9d0541763ae6b2
Steve Turley <steve@turleylabs.com>
2020-09-23 10:26:37 -0500

    Initial Commit

---
+++ src/test/java/com/turleylabs/gildedrose/GildedRoseTest.java
@@ -0,0 +1,17 @@
+package com.turleylabs.gildedrose;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.QuietReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@UseReporter(QuietReporter.class)
+class GildedRoseTest {
+
+    @Test
+    void approveGildedRose() {
+    }
+}
